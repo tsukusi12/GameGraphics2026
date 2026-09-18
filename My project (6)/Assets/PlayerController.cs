34f32eb0f017540fad92f9b96c6abbcfff076d41
tsukusi12 <chaewon7725@gmail.com>
2026-09-18 12:35:00 +0900

    3주차

--- My project (6)/Assets/PlayerController.cs
+++ My project (6)/Assets/PlayerController.cs
@@ -6,21 +6,40 @@ public class PlayerController : MonoBehaviour
   public string playerName = "Player";
   public int hp = 100;
   public float moveSpeed = 5f;
+  public float jumpPower = 8f;
+  public Vector3 startPosition;
+  public Transform visual;
   private Vector2 moveInput;
-
+  private Rigidbody2D rb;
 
   void Start()
   {
-     Debug.Log(playerName + " 시작. 체력 " + hp);
+    rb = GetComponent<Rigidbody2D>();
+    transform.position = startPosition;
+    Debug.Log(playerName + " 시작. 체력 " + hp);
+    Debug.Log("피격 후 체력 " + (hp - 30));
+    Debug.Log("달리기 속도 " + moveSpeed * 2);
+    Debug.Log("10 / 4 = " + 10 / 4);
   }
   void OnMove(InputValue value)
   {
-
     moveInput = value.Get<Vector2>();
+    if (moveInput.x > 0)
+    { visual.localScale = new Vector3(1, 1, 1); }
+    else if (moveInput.x < 0)
+    { visual.localScale = new Vector3(-1, 1, 1); }
   }
- 
+  void OnJump(InputValue value)
+  {
+    if (value.isPressed)
+    {
+      Debug.Log("점프!");
+      rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+    }
+  }
   void Update()
   {
+    Debug.Log("Update");
     transform.Translate(Vector3.right * moveInput.x * moveSpeed * Time.deltaTime);
   }
 }
